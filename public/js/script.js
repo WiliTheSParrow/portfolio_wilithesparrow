e2f314616e200fad52ec30d5e27a6dd8b4516c67
Julia W <w.julia91@gmail.com>
2020-06-25 16:49:40 +0200

    init Vue

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,21 @@
-/* 
-What needs to be done?
-- minden adat JSON-bol legyen kiiratva (szoveg, kepek)
-- a JSON-bol iratodjon ki az osszes kartya
+var app = new Vue({
+    el: '#app',
+    data: {}
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
