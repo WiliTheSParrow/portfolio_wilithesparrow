4ff5c768108a2e458f8cda230a98f4bfa6544648
Julia W <w.julia91@gmail.com>
2020-06-26 15:33:15 +0200

    setting up JSON file fetch

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,23 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
+            el: '#app',
+            data: {
+                message: 'Hello Vue!',
+                myData: [],
+                projects: []
+            },
+
+            methods: {
+                // fetch data from JSON files:
+                getLocalData: function (link, destination) {
+                    fetch(link, {
+                            method: "GET",
+                        })
+                        .then(response => response.json())
+                        .then(json => {
+                            app[destination] = json;
+                        })
+                        .catch(error => error);
+                },
 
 
 
@@ -17,7 +32,4 @@ var app = new Vue({
 
 
 
-
-
-
-})
+            })
