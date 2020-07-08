33ef9ee449263085fa8b7f9b9315a40287f7655a
Julia W <w.julia91@gmail.com>
2020-07-08 12:57:25 +0200

    animation attempt

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,3 @@
-/* import ScrollAnimation from './scrollanimation'
-
-Vue.directive('scrollanimation', ScrollAnimation); */
-
-const animatedScrollObserver = new IntersectionObserver(
-    (entries, animatedScrollObserver) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('enter');
-                animatedScrollObserver.unobserve(entry.target);
-            }
-        });
-    });
-
 var app = new Vue({
 
     el: '#app',
@@ -28,11 +14,6 @@ var app = new Vue({
         this.getLocalData('projects.json', 'projects');
 
 
-    },
-
-    bind(el) {
-        el.classList.add('before-enter');
-        animatedScrollObserver.observe(el);
     },
 
     methods: {
