12a623dbf83a49429bbbcd4a437c2a6572cb385e
Julia W <w.julia91@gmail.com>
2020-07-08 10:52:45 +0200

    scrollanimation js file added

---
+++ public/js/scrollanimation.js
@@ -0,0 +1,17 @@
+const animatedScrollObserver = new IntersectionObserver(
+    (entries, animatedScrollObserver) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('enter');
+                animatedScrollObserver.unobserve(entry.target);
+            }
+        });
+    });
+
+export default {
+    // This function is called only once when the directive is bound to the element: 
+    bind(el) {
+        el.classList.add('before-enter');
+        animatedScrollObserver.observe(el);
+    }
+}
